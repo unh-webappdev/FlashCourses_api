12889018f028d26e41baf4c01b4e045326a1905c
Andrea <andrealindamurphy@gmail.com>
2018-03-20 18:53:43 -0400

    Models.py

--- Flash_course/src/courses/models.py
+++ Flash_course/src/courses/models.py
@@ -3,15 +3,15 @@ Course models for FlashCourse application
 Database: FlashCourses- mySQL
 """
 from django.db import models
-<<<<<<< HEAD
+
 import uuid
 
-class Course(models.Model):
-    course_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-class College(models.Model):
+class Instution(models.Model):
     ipeds = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField
     location = models.CharField
-=======
->>>>>>> 040e10a905c2a95c6e0ee1b105fffa607719a7cb
+
+class Course(models.Model):
+    course_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    parent_institution = models.ForeignKey(on_delete=models.CASCADE, default=1)
