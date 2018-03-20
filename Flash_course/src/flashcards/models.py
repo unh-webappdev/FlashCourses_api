c1605961d97444c153e892449e93752087d1b9a3
Andrea <andrealindamurphy@gmail.com>
2018-03-20 19:49:45 -0400

    new app

---
+++ Flash_course/src/flashcards/models.py
@@ -0,0 +1,21 @@
+"""
+Flash card models for FlashCourses application
+Database: FlashCourses- mySQL
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from courses.models import Course
+
+import uuid
+
+class Deck(models.Model):
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    parent_user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    parent_course = models.ForeignKey(Course, on_delete=models.CASCADE, default=1)
+    title = models.CharField(max_length=64, null=False, blank=False)
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
