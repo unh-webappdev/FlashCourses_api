56717a423ecc1da9e6381795333a35ddbb4c29b9
Andrea <andrealindamurphy@gmail.com>
2018-03-23 17:00:43 -0400

    Updated models

--- Flash_course/src/accounts/models.py
+++ Flash_course/src/accounts/models.py
@@ -6,12 +6,13 @@ Database: FlashCourses- mySQL
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.validators import ASCIIUsernameValidator
-from courses.models import Instution, Course
+
+from courses.models import Institution
 
 import uuid
 
 class UserProfile(models.Model):
     user_id = models.AutoField(auto_created=True, primary_key=True)
     parent_user = models.ForeignKey(User,  on_delete=models.CASCADE, default=1)
-    parent_institution = models.ForeignKey(Instution, on_delete=models.CASCADE, default=1)
+    parent_institution = models.ForeignKey(Institution, on_delete=models.CASCADE, default=1)
 
