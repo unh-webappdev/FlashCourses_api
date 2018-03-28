3bde388b6e94d2f4256049c1a5711a9369513bf9
mce123 <pmcelhiney@mce123.com>
2018-03-27 20:16:06 -0400

    Updates to API, including Authentication with JWT

--- Flash_course/src/FlashCourses/settings.py
+++ Flash_course/src/FlashCourses/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
