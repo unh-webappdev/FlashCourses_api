7f0553fc61808ac06edda9a50ea12f1f026bac0b
Andrea <andrealindamurphy@gmail.com>
2018-04-05 13:43:47 -0400

    Update admin

--- flash/src/courses/admin.py
+++ flash/src/courses/admin.py
@@ -6,15 +6,6 @@ from django.contrib import admin
 from .models import Institution, Course
 
 
-
-class InstitutionAdmin(admin.ModelAdmin):
-    list_display= ('ipeds', 'institution_name', 'location')
-
-
-class CourseAdmin(admin.ModelAdmin):
-    list_display= ('course_id', 'parent_institution')
-
-
-admin.site.register(Institution, InstitutionAdmin)
-admin.site.register(Course, CourseAdmin)
+admin.site.register(Institution)
+admin.site.register(Course)
 
