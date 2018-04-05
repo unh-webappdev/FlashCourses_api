c7ddcb4d6d69b2e51c4043ac6aa1bdad6c9346f3
Andrea <andrealindamurphy@gmail.com>
2018-04-05 12:00:00 -0400

    update admin for applications

--- flash/src/flashcards/admin.py
+++ flash/src/flashcards/admin.py
@@ -6,12 +6,6 @@ from django.contrib import admin
 
 from .models import Card, Deck
 
-class CardAdmin(admin.ModelAdmin):
-    list_display= ()
 
-class DeckAdmin(admin.ModelAdmin):
-    list_display= ()
-
-
-admin.site.register(Card, CardAdmin)
-admin.site.register(Deck, DeckAdmin)
+admin.site.register(Card)
+admin.site.register(Deck)
